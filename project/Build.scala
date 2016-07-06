7e025e6c76930c75a662d86d1946ad52ce79ad37
edeustace <ed.eustace@gmail.com>
2016-07-06 11:36:22 +0100

    small tweaks

--- project/Build.scala
+++ project/Build.scala
@@ -9,19 +9,20 @@ object Build extends sbt.Build{
   lazy val macros = Project("macros", file("macros"))
       .settings(
         libraryDependencies <+= (scalaVersion)( "org.scala-lang" % "scala-reflect" % _),
-        releaseVersionBump := Bump.Minor,
         publishTo := authPublishTo.value
       )
 
   lazy val test = Project("macros-test", file("test"))
     .settings(
       publish := false,
+      publishTo := authPublishTo.value,
       libraryDependencies += "org.specs2" %% "specs2-core" % "3.3.1" % "test"
     )
     .dependsOn(macros)
 
   lazy val root = Project("macros-root", file("."))
       .settings(
+        releaseVersionBump := Bump.Minor,
         publishTo := authPublishTo.value,
         publish := false
       )
